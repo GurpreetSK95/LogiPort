9092b8ef91962e778bb85ae4e7485ceaee0e4142
Gurpreet Singh <gurpreetsk@hotmail.com>
2016-01-22 21:44:28 +0530

    App now working on real device. Main Screen yet to be configured.

--- app/src/main/java/com/gurpreetsingh/www/logiporttestapp/LoginActivity.java
+++ app/src/main/java/com/gurpreetsingh/www/logiporttestapp/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.gurpreetsingh.www.logiporttestapp;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +21,7 @@ public class LoginActivity extends AppCompatActivity {
 
     String loginID;
     String password;
+    ProgressDialog progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,33 +36,55 @@ public class LoginActivity extends AppCompatActivity {
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(final View v) {
+            public void onClick(View v) {
 
-                ProgressDialog progress = new ProgressDialog(LoginActivity.this);
-                progress.setTitle("Logging in...");
-                progress.setMessage("Please be patient");
-                progress.show();
+                ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+                if (networkInfo != null && networkInfo.isConnected()) {
 
-                loginID = loginET.getText().toString();
-                password = passwordET.getText().toString();
+                    loginID = loginET.getText().toString();
+                    password = passwordET.getText().toString();
 
-                int idLength = checkLength(loginID);
-                int pwLength = checkLength(password);
-                if (idLength == 0 || pwLength == 0) {
-                    Toast.makeText(LoginActivity.this, "Please enter Information", Toast.LENGTH_SHORT).show();
-                } else {
-                    ParseUser.logInInBackground(loginID, password, new LogInCallback() {
-                        @Override
-                        public void done(ParseUser user, ParseException e) {
-                            if (e != null) {
-                                Snackbar.make(v, "Wrong Credentials\nPlease try again", Snackbar.LENGTH_LONG).show();
-                                e.printStackTrace();
-                            } else {
-                                Intent intent = new Intent(LoginActivity.this, MainScreen.class);
-                                startActivity(intent);
+                    int idLength = checkLength(loginID);
+                    int pwLength = checkLength(password);
+                    if (idLength == 0 || pwLength == 0) {
+                        Toast.makeText(LoginActivity.this, "Please enter Information", Toast.LENGTH_SHORT).show();
+                    } else {
+                        progress = new ProgressDialog(LoginActivity.this);
+                        progress.setTitle("Logging in...");
+                        progress.setMessage("Please be patient");
+                        progress.show();
+                        ParseUser.logInInBackground(loginID, password, new LogInCallback() {
+                            @Override
+                            public void done(ParseUser user, ParseException e) {
+                                if (e != null) {
+                                    Toast.makeText(LoginActivity.this
+                                            , "Wrong Credentials\nPlease try again", Toast.LENGTH_SHORT).show();
+                                    progress.dismiss();
+                                    e.printStackTrace();
+                                } else {
+                                    Intent intent = new Intent(LoginActivity.this, MainScreen.class);
+                                    startActivity(intent);
+                                    progress.dismiss();
+                                }
                             }
-                        }
-                    });
+                        });
+                    }
+                } else {
+                    AlertDialog.Builder builder1 = new AlertDialog.Builder(LoginActivity.this);
+                    builder1.setMessage("Please connect to network");
+                    builder1.setCancelable(true);
+                    builder1.setIcon(android.R.drawable.ic_dialog_alert);
+                    builder1.setNeutralButton(
+                            "Dismiss",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            });
+
+                    AlertDialog alert11 = builder1.create();
+                    alert11.show();
                 }
 
             }
--- app/src/main/java/com/gurpreetsingh/www/logiporttestapp/MainScreen.java
+++ app/src/main/java/com/gurpreetsingh/www/logiporttestapp/MainScreen.java
@@ -14,6 +14,7 @@ public class MainScreen extends AppCompatActivity {
         setContentView(R.layout.activity_main_screen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
     /*    Splash.loginObject.saveInBackground(new SaveCallback() {
             public void done(ParseException e) {
                 if (e == null) {
