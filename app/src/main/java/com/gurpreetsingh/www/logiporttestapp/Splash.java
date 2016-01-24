8142cf36ade52d3693121f8129cec2b584ad7a89
Gurpreet Singh <gurpreetsk@hotmail.com>
2016-01-24 11:35:24 +0530

    App now working on real device. Completed. Made some small improvements.

--- app/src/main/java/com/gurpreetsingh/www/logiporttestapp/Splash.java
+++ app/src/main/java/com/gurpreetsingh/www/logiporttestapp/Splash.java
@@ -2,6 +2,7 @@ package com.gurpreetsingh.www.logiporttestapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
@@ -17,38 +18,43 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (ParseUser.getCurrentUser() != null) {
+                    intent = new Intent(Splash.this, MainScreen.class);
+                    startActivity(intent);
+                } else {
 
-        if (ParseUser.getCurrentUser() != null) {
-            intent = new Intent(this, MainScreen.class);
-            startActivity(intent);
-        } else {
-
-            //Made to register user on first run: UserName and Password pre-defined
-            boolean firstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE).getBoolean("firstRun", true);
-            if (firstRun) {
-                ParseUser logiport = new ParseUser();
-                logiport.setUsername("task");
-                logiport.setPassword("logitask");
-                logiport.signUpInBackground(new SignUpCallback() {
-                    @Override
-                    public void done(ParseException e) {
-                        if (e == null) {
-                            Toast.makeText(Splash.this, "SIGN-UP SUCCESSFUL!", Toast.LENGTH_SHORT).show();
-                        } else {
-                            e.printStackTrace();
-                        }
+                    //Made to register user on first run: UserName and Password pre-defined
+                    boolean firstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE).getBoolean("firstRun", true);
+                    if (firstRun) {
+                        ParseUser logiport = new ParseUser();
+                        logiport.setUsername("task");
+                        logiport.setPassword("logitask");
+                        logiport.signUpInBackground(new SignUpCallback() {
+                            @Override
+                            public void done(ParseException e) {
+                                if (e == null) {
+                                    Toast.makeText(Splash.this, "SIGN-UP SUCCESSFUL!", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                        // Save the state
+                        getSharedPreferences("PREFERENCE", MODE_PRIVATE)
+                                .edit()
+                                .putBoolean("firstRun", false)
+                                .commit();
                     }
-                });
-                // Save the state
-                getSharedPreferences("PREFERENCE", MODE_PRIVATE)
-                        .edit()
-                        .putBoolean("firstRun", false)
-                        .commit();
+                    intent = new Intent(Splash.this, LoginActivity.class);
+                    startActivity(intent);
+                }
             }
+        });
 
-            intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
-        }
     }
 
 
